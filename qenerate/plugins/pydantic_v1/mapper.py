ebbd6d5ba863c1d5c782bc66953c2b5ec6ed7aa0
Karl Fischer <fishi0x01@users.noreply.github.com>
2022-06-27 15:17:11 +0200

    enhance mapper functions (#12)

--- qenerate/plugins/pydantic_v1/mapper.py
+++ qenerate/plugins/pydantic_v1/mapper.py
@@ -4,9 +4,29 @@ from graphql import GraphQLOutputType
 
 
 def _keyword_sanitizer(s: str) -> str:
-    if s in ("global", "from", "type", "id", "to", "format"):
-        return f"f_{s}"
+    keyword_remapping = {
+        "global": "q_global",
+        "from": "q_from",
+        "type": "q_type",
+        "id": "q_id",
+        "to": "q_to",
+        "format": "q_format",
+    }
+    if s in keyword_remapping:
+        return keyword_remapping[s]
     return s
+
+
+def graphql_class_name_to_python(graphql_type: GraphQLOutputType) -> str:
+    class_name = str(graphql_type)
+    result = class_name[0]
+    for i in range(1, len(class_name)):
+        cur, prev = class_name[i], class_name[i - 1]
+        if cur != "_" and prev != "_":
+            result += cur
+        elif cur != "_" and prev == "_":
+            result += cur.upper()
+    return result
 
 
 def graphql_primitive_to_python(graphql_type: GraphQLOutputType) -> str:
@@ -23,8 +43,17 @@ def graphql_primitive_to_python(graphql_type: GraphQLOutputType) -> str:
 
 
 def graphql_field_name_to_python(name: str) -> str:
+    # ElbFQDN -> ["elb", "f", "q", "d", "n"]
     parts = re.split("(?=[A-Z])", name)
     for i, el in enumerate(parts):
         parts[i] = el.lower()
 
-    return _keyword_sanitizer("_".join(parts))
+    # ["elb", "f", "q", "d", "n"] -> elb_fqdn
+    result = parts[0]
+    for i in range(1, len(parts)):
+        cur, prev = parts[i], parts[i - 1]
+        if len(cur) == 1 and len(prev) == 1:
+            result += cur
+        else:
+            result += f"_{cur}"
+    return _keyword_sanitizer(result)
---
+++ tests/plugins/pydantic_v1/test_difficult_attribute_name.py
@@ -0,0 +1,13 @@
+from qenerate.plugins.pydantic_v1.plugin import PydanticV1Plugin
+from tests.util import get_introspection, get_query, get_result
+
+
+def test_difficult_attribute_name_query():
+    schema_raw = get_introspection()
+    query = get_query(filename="difficult_attribute_name.gql")
+    plugin = PydanticV1Plugin()
+    result = plugin.generate(query=query, raw_schema=schema_raw)
+    assert result == get_result(
+        plugin="pydantic_v1",
+        filename="difficult_attribute_name.py.txt",
+    )
