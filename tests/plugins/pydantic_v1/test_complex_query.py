ac529dffb22bf5bbb655b8a0bf5cadf754ef3852
Karl Fischer <kfischer@redhat.com>
2022-07-04 15:53:41 +0200

    APPSRE-5046 add convenience function

--- tests/plugins/pydantic_v1/test_complex_query.py
+++ tests/plugins/pydantic_v1/test_complex_query.py
@@ -1,12 +1,12 @@
 from qenerate.plugins.pydantic_v1.plugin import PydanticV1Plugin
-from tests.util import get_introspection, get_query, get_result
+from tests.util import get_introspection, get_result
 
 
 def test_interface_valid_query():
     schema_raw = get_introspection()
-    query = get_query(filename="complex_inline_fragments_and_collisions.gql")
+    query_file = "tests/queries/complex_inline_fragments_and_collisions.gql"
     plugin = PydanticV1Plugin()
-    result = plugin.generate(query=query, raw_schema=schema_raw)
+    result = plugin.generate(query_file=query_file, raw_schema=schema_raw)
     assert result == get_result(
         plugin="pydantic_v1",
         filename="complex_inline_fragments_and_collisions.py.txt",
@@ -15,9 +15,9 @@ def test_interface_valid_query():
 
 def test_interface_humongous_query():
     schema_raw = get_introspection()
-    query = get_query(filename="saas_humongous.gql")
+    query_file = "tests/queries/saas_humongous.gql"
     plugin = PydanticV1Plugin()
-    result = plugin.generate(query=query, raw_schema=schema_raw)
+    result = plugin.generate(query_file=query_file, raw_schema=schema_raw)
     assert result == get_result(
         plugin="pydantic_v1",
         filename="saas_humongous.py.txt",
--- tests/plugins/pydantic_v1/test_interface_valid_query.py
+++ tests/plugins/pydantic_v1/test_interface_valid_query.py
@@ -1,12 +1,12 @@
 from qenerate.plugins.pydantic_v1.plugin import PydanticV1Plugin
-from tests.util import get_introspection, get_query, get_result
+from tests.util import get_introspection, get_result
 
 
 def test_interface_valid_query():
     schema_raw = get_introspection()
-    query = get_query(filename="ocp_with_inline_fragments.gql")
+    query_file = "tests/queries/ocp_with_inline_fragments.gql"
     plugin = PydanticV1Plugin()
-    result = plugin.generate(query=query, raw_schema=schema_raw)
+    result = plugin.generate(query_file=query_file, raw_schema=schema_raw)
     assert result == get_result(
         plugin="pydantic_v1",
         filename="ocp_with_inline_fragments.py.txt",
