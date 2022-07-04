abae8876c513825a1cac5f8b3a0e29a15b52f327
Karl Fischer <fishi0x01@users.noreply.github.com>
2022-07-04 15:56:13 +0200

    APPSRE-5046 add convenience function (#21)

--- tests/plugins/pydantic_v1/test_invalid_query.py
+++ tests/plugins/pydantic_v1/test_invalid_query.py
@@ -5,20 +5,20 @@ from qenerate.plugins.pydantic_v1.plugin import (
     InvalidQueryError,
     PydanticV1Plugin,
 )
-from tests.util import get_introspection, get_query
+from tests.util import get_introspection
 
 
 def test_anonymous_query():
     schema_raw = get_introspection()
-    query = get_query(filename="anonymous_query.gql")
+    query_file = "tests/queries/anonymous_query.gql"
     plugin = PydanticV1Plugin()
     with pytest.raises(AnonymousQueryError):
-        plugin.generate(query=query, raw_schema=schema_raw)
+        plugin.generate(query_file=query_file, raw_schema=schema_raw)
 
 
 def test_invalid_query():
     schema_raw = get_introspection()
-    query = get_query(filename="invalid_query.gql")
+    query_file = "tests/queries/invalid_query.gql"
     plugin = PydanticV1Plugin()
     with pytest.raises(InvalidQueryError):
-        plugin.generate(query=query, raw_schema=schema_raw)
+        plugin.generate(query_file=query_file, raw_schema=schema_raw)
--- tests/util.py
+++ tests/util.py
@@ -1,11 +1,6 @@
 import json
 
 
-def get_query(filename: str) -> str:
-    with open(f"tests/queries/{filename}") as f:
-        return f.read()
-
-
 def get_result(plugin: str, filename: str) -> str:
     with open(f"tests/queries/expected/{plugin}/{filename}") as f:
         return f.read()
