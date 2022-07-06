cd5e7657152124c5eb4b4cfdfdbe22df46707101
Karl Fischer <kfischer@redhat.com>
2022-07-06 10:37:58 +0200

    APPSRE-5946

--- build_deploy.sh
+++ build_deploy.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-echo TODO
+set -e
+
+python3 -m pip install --user twine wheel
+python3 setup.py bdist_wheel
+
+# TODO: this fails if the package version alrdy exists
+# We should check before uploading
+python3 -m twine upload dist/* || true
