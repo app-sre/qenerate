864e5a0c26a82644bd9c5bcb4b874df8c9dfc589
Karl Fischer <fishi0x01@users.noreply.github.com>
2022-08-17 16:39:48 +0200

    APPSRE-5972 fix nullables in list and operation type (#32)

--- tests/core/test_unwrapper.py
+++ tests/core/test_unwrapper.py
@@ -25,7 +25,7 @@ from graphql import (
         [
             GraphQLNonNull(GraphQLList(GraphQLObjectType(name="MyObject", fields=[]))),
             UnwrapperResult(
-                wrapper_stack=[WrapperType.LIST, WrapperType.OPTIONAL],
+                wrapper_stack=[WrapperType.LIST],
                 inner_gql_type=GraphQLObjectType(name="MyObject", fields=[]),
                 is_primitive=False,
             ),
@@ -45,7 +45,6 @@ from graphql import (
                     WrapperType.OPTIONAL,
                     WrapperType.LIST,
                     WrapperType.LIST,
-                    WrapperType.OPTIONAL,
                 ],
                 inner_gql_type=GraphQLScalarType(name="Integer"),
                 is_primitive=True,
