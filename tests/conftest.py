1f9bd3d62ef25ea55cb82ce64223b169faf7b805
Karl Fischer <kfischer@redhat.com>
2022-08-24 11:27:05 +0200

    APPSRE-5972 enhance test suite

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,15 +10,13 @@ from qenerate.core.plugin import GeneratedFile
 
 @pytest.fixture()
 def expected_files():
-    """Read fixture files from test/KIND/expected/PLUGIN/DEFINITION and remove .txt suffix."""
+    """Read fixture files from tests/definitions/expected/PLUGIN/CASE and remove .txt suffix."""
 
-    def _(plugin: str, kind: str, definition: str) -> list[GeneratedFile]:
+    def _(plugin: str, case: str) -> list[GeneratedFile]:
         return sorted(
             [
                 GeneratedFile(file=f.with_suffix(""), content=f.read_text())
-                for f in Path(f"tests/{kind}/expected/{plugin}/{definition}").glob(
-                    "**/*"
-                )
+                for f in Path(f"tests/generator/expected/{plugin}/{case}").glob("**/*")
             ]
         )
 
@@ -27,6 +25,6 @@ def expected_files():
 
 @pytest.fixture()
 def schema():
-    with open("tests/queries/introspection.json") as f:
+    with open("tests/generator/introspection.json") as f:
         raw_schema = json.loads(f.read())["data"]
     return build_client_schema(cast(IntrospectionQuery, raw_schema))
