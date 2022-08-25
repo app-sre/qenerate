b9d823823a23227f2f60ef69246b2a2ebf148601
Karl Fischer <fishi0x01@users.noreply.github.com>
2022-08-25 14:55:23 +0200

    APPSRE-5972 fix imports (#45)

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ entry_points = {"console_scripts": ["qenerate = qenerate.cli:run"]}
 
 setup_kwargs = {
     "name": "qenerate",
-    "version": "0.2.0",
+    "version": "0.2.1",
     "description": "Code Generator for GraphQL Query Data Classes",
     "long_description": None,
     "author": "Service Delivery - AppSRE",
